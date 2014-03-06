1911f44626a34745118cce1a38a540c9ac8391a8
n352991 <n352991@0051-162943-CTE.student.sarasota.k12.fl.us>
2014-03-06 12:51:37 -0500

    Added choice of player 2 and fixed turn logic

--- src/pig/game.java
+++ src/pig/game.java
@@ -8,11 +8,25 @@ package pig;
 import java.util.Scanner;
 public class game extends pigDice{
     public static void main(String[]args){
-        System.out.println("What is your name?");
+        String opponent;
+        System.out.println("Hello player! What is your name?");
         String name = new Scanner(System.in).nextLine();
         System.out.println("Ok "+name+", let me get the dice set up...");
         System.out.println("The name of the game is pig, would you like to play? (y/n)");
         String play = new Scanner(System.in).nextLine();
+        System.out.println("Do you want to play against an AI, another player, or Caleb? (a/p/c)");
+        String p2 = new Scanner(System.in).nextLine();
+        if(p2.equalsIgnoreCase("a")){
+            System.out.println("A.I created...");
+            opponent="A.I";
+        }else if(p2.equalsIgnoreCase("p")){
+            System.out.println("Welcome Player 2, what is your name?");
+            opponent = new Scanner(System.in).nextLine();
+            System.out.println("Hello "+opponent+",  let's begin...");
+        }else if(p2.equalsIgnoreCase("c")){
+            System.out.println("Launching A.I: Caleb");
+            opponent="Caleb";
+        }
         String playValue;
         String rollValue;
         int currentScore=0;
@@ -45,9 +59,9 @@ public class game extends pigDice{
             System.out.println("d1 score: "+roll1);
             System.out.println("d2 score: "+roll2);
         System.out.println("about to check for ones...");
-        if(roll1==0||roll2==0){
+        if(roll1==0||roll2==0&&turn==1){
             System.out.println("rolled a one -- turn over. \r\n setting turn to 2");
-            turn=1;
+            turn=2;
             roll1=0;
             roll2=0;
         }
@@ -68,9 +82,11 @@ public class game extends pigDice{
            roll1 = d1.score();
            roll2 = d2.score();
            System.out.println("about to check for ones...");
-           if(roll1==0||roll2==0){
-               System.out.println("rolled a one -- turn over.\r\n setting turn to 1");
-            turn=2;
+           if(roll1==0||roll2==0&&turn==2){
+            System.out.println("rolled a one -- turn over.\r\n setting turn to 1");
+            turn=1;
+            roll1=0;
+            roll2=0;
            } 
            currentScore = currentScore+roll1+roll2;
            System.out.println("Current Score is: "+currentScore);
